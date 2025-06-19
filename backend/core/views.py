34434d441c8294b121defee7cdec1d13e5c8de3c
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-20 02:37:37 +0800

    made the role into dynamic when registering a user

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,20 +3,25 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .models import Auth0User
 from .auth import get_token_auth_header, decode_jwt
+from core.constants import ALLOWED_ROLES
 
-class RegisterAuth0User(APIView):
+class RegisterUser(APIView):
   permission_classes = [AllowAny]
   
   def post(self, request):
     token = get_token_auth_header(request)
     payload = decode_jwt(token)
     
+    role = payload.get('role', 'student')
+    if role not in ALLOWED_ROLES:
+      role = 'student'
+    
     user, created = Auth0User.objects.get_or_create(
       auth0_id = payload['sub'],
       defaults={
         'email': payload['email'],
         'name': payload.get('name', ''),
-        'role': 'student'  # default role
+        'role': role
       }
     )
     
