db6acfbca91a645349225df9ce4c5a28850e7eb4
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-21 06:07:30 +0800

    added sampel pages

---
+++ frontend/src/pages/Explore.tsx
@@ -0,0 +1,5 @@
+function Explore() {
+  return <div className="text-xl">Explore Mock Exams</div>;
+}
+
+export default Explore;
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div className="text-red-500 text-center">404: Page Not Found</div>;
+}
+
+export default NotFound;
