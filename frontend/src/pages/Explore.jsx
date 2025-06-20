ca28ac4023d35f2858ee3e5f057cd70ce0e03ce0
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-21 05:51:42 +0800

    re initialized project, js to ts

--- frontend/src/pages/Explore.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Explore() {
-  return <div className="text-xl">Explore Quizora</div>;
-}
-
-export default Explore;
--- frontend/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NotFound() {
-  return <div className="text-red-500 text-center">404: Page Not Found</div>;
-}
-
-export default NotFound;
