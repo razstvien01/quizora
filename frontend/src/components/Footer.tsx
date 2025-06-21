149e8669ce1347bca75af4841660f6484ddad580
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-21 11:05:29 +0800

    added footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center py-4 text-sm">
+      @ {new Date().getFullYear()} Quizora. All rights reserved.
+    </footer>
+  );
+}
