08cbc359dc4f0361b618aad4cd64e38d8cacbf53
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-22 18:41:05 +0800

    added a route for the auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/theme-provider";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </ThemeProvider>
   );
