65497c9745733752346922565a5f442bef0432f7
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-24 10:16:00 +0800

    created dashboard page

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function Dashboard() {
+  const { user } = useAuth0();
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Welcome, {user?.name}!</h1>
+      <p className="mt-4">This is your Quizora dashboard. 🎓</p>
+    </div>
+  );
+}
+
+export default Dashboard;
