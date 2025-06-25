7c3facfb6638d1381253cbd57692684784e4a755
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-25 12:17:37 +0800

    feat(dashboard): used useAuthRedirect to redirect to the home page if not auth likewise in the dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
+import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Dashboard() {
+  useAuthRedirect({ requireAuth: true });
+
   const { user } = useAuth0();
 
   return (
