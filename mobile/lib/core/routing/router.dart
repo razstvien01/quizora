1c23c1d569723d2a2e4d47f59de84e4e47c44fff
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-06-29 23:24:44 +0800

    feat(router): replaced hardcoded routes to AppRoutes static and modified redirect func logic

--- mobile/lib/core/routing/router.dart
+++ mobile/lib/core/routing/router.dart
@@ -5,6 +5,9 @@ import 'package:mobile/core/presentation/screens/splash_screen.dart';
 import 'package:mobile/features/dashbaord/presentation/screens/dashboard_screen.dart';
 import 'package:mobile/features/auth/presentation/screens/login_screen.dart';
 
+import 'package:mobile/core/constants/app_routes.dart';
+import 'package:mobile/features/landing/presentation/screens/landing_screen.dart';
+
 final authProvider = StateProvider<bool>((ref) => false);
 
 final routerProvider = Provider<GoRouter>((ref) {
@@ -14,18 +17,34 @@ final routerProvider = Provider<GoRouter>((ref) {
     initialLocation: '/',
     debugLogDiagnostics: true,
     routes: [
-      GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
-      GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
       GoRoute(
-        path: '/dashboard',
+        path: AppRoutes.splash,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.login,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.dashboard,
         builder: (context, state) => const DashboardScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.landing,
+        builder: (context, state) => const LandingScreen(),
       ),
     ],
     redirect: (context, state) {
-      final loggingIn = state.fullPath == '/login';
+      final goingToLanding = state.fullPath == AppRoutes.landing;
+      final goingToLogin = state.fullPath == AppRoutes.login;
 
-      if (!isLoggedIn) return loggingIn ? null : '/login';
-      if (loggingIn) return '/dashboard';
+      if (!isLoggedIn) {
+        //* Allows landing and login for unauthenticated users
+        if (goingToLanding || goingToLogin) return null;
+        return AppRoutes.landing;
+      }
+
+      if (goingToLanding || goingToLogin) return AppRoutes.dashboard;
 
       return null;
     },
