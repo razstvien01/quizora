4b30c9648777f2dd6f02d4d1de77f6723c211506
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-02 09:55:49 +0800

    fix: AUTH0_DOMAIN typo

--- mobile/lib/core/utils/env.dart
+++ mobile/lib/core/utils/env.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
   static String get apiUrl => dotenv.env['API_BASE_URL']!;
-  static String get auth0Domain => dotenv.env['AUTH_DOMAIN']!;
+  static String get auth0Domain => dotenv.env['AUTH0_DOMAIN']!;
   static String get auth0ClientId => dotenv.env['AUTH0_CLIENT_ID']!;
   static String get auth0RedirectUri => dotenv.env['AUTH0_REDIRECT_URI']!;
 }
