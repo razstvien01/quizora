6102b516f8bed8df6aabc012bfb74cc0ec4ee8be
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-02 11:41:03 +0800

    implemented sample user api

---
+++ mobile/lib/features/user/data/user_api.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:mobile/core/network/dio_client.dart';
+
+class UserApi {
+  Future<Map<String, dynamic>> getCurrentUser() async {
+    try {
+      final res = await DioClient.dio.get('/uers/me/');
+      return res.data;
+    } on DioException catch (e) {
+      throw e.response?.data['details'] ?? 'Unknown error occurred';
+    }
+  }
+}
