17a0b1faee5d7624d11d702fbf04fb6a4da62894
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-08 13:16:12 +0800

    feat: setup the fcm servicew

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quizora/config/app_theme.dart';
 import 'package:quizora/router/router.dart';
+import 'package:quizora/services/firebase_messaging_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
+  await initFCM();
 
   runApp(const ProviderScope(child: QuizoraApp()));
 }
@@ -18,12 +22,12 @@ class QuizoraApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-    
+
     return MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.light,
-        darkTheme: AppTheme.dark,
-        routerConfig: router,
-      );
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      routerConfig: router,
+    );
   }
 }
