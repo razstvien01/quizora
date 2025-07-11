912ff6f8b6d4651664bdc2319da87e5282a038df
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-11 14:47:18 +0800

    feat: implemented user model

---
+++ backend/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+class User(models.Model):
+  id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  auth_id = models.CharField(max_length=128, unique=True)
+  email = models.EmailField()
+  name = models.CharField(max_length=255)
+  role = models.CharField(max_length=64, default='student')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return f"User {self.email} ({self.role})"
