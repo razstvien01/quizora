909e6987f2aa51296cc7f86793cf035341082181
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 01:37:06 +0800

    chore: removed unused import

--- backend/tags/models/tag.py
+++ backend/tags/models/tag.py
@@ -1,8 +1,6 @@
 import uuid
 from django.db import models
 
-from backend.mock_quizzes.models import MockQuiz
-
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
