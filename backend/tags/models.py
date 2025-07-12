540cdaf67095fa04068214f07399dc4dd4b13a5c
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 00:30:22 +0800

    feat: implemented tag and mockquiztag model schema

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from backend.mock_quizzes.models import MockQuiz
+
+
+class Tag(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=50, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return f"Tag {self.id} - {self.name}"
+  
+class MockQuizTag(models.Model):
+  mock_quiz = models.ForeignKey(MockQuiz, on_delete=models.CASCADE)
+  tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return f"MockQuizTag {self.mock_quiz} - {self.tag}"
+  
+  class Meta:
+    unique_together = ('mock_quiz', 'tag')
