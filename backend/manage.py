76a8c2e17ef4ce879b91dc4ed6982bbc01f6c30c
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 01:34:09 +0800

    chore: modified DJANGO_SETTINGS_MODULE value to dev

--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,11 @@
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
