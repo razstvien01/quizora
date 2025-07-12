e1efdcf4306ef37f93a583f6b3beb16b33636db2
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 00:45:31 +0800

    chore: split the mock quiz tag and tag schema model file in the tags/models/

--- backend/tags/models/tag.py
+++ backend/tags/models/tag.py
@@ -3,7 +3,6 @@ from django.db import models
 
 from backend.mock_quizzes.models import MockQuiz
 
-
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
@@ -12,13 +11,3 @@ class Tag(models.Model):
     
     def __str__(self):
         return f"Tag {self.id} - {self.name}"
-    
-class MockQuizTag(models.Model):
-    mock_quiz = models.ForeignKey(MockQuiz, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"MockQuizTag {self.mock_quiz} - {self.tag}"
-    
-    class Meta:
-        unique_together = ('mock_quiz', 'tag')
