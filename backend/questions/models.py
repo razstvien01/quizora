4c2dd0c40478cb7b345d6b57371c0f38996ece26
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 02:08:38 +0800

    chore: modified classname to singular

--- backend/questions/models.py
+++ backend/questions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from mock_quizzes.models import MockQuiz
 from django.core.exceptions import ValidationError
 
-class Questions(models.Model):
+class Question(models.Model):
     QUESTION_TYPES = (
         ('MCQ', 'Multiple Choice'),
         ('TF', 'True/False'),
