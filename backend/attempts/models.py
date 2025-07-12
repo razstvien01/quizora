54e4b3cd011abf0bd2dc2fce2ce24be94c53ab17
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 01:31:02 +0800

    feat: defined attemp schema model

--- backend/attempts/models.py
+++ backend/attempts/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from mock_quizzes.models import MockQuiz
+from users.models import User
+
+class Attempt(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, related_name='attempts', on_delete=models.CASCADE)
+    mock_quiz = models.ForeignKey(MockQuiz, related_name='attempts', on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    started_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    answers = models.JSONField()
