98abd20f24fec818d78dbebbe02288d23cc7e690
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 02:37:35 +0800

    chore: added django_extensions in the INSTALLED_APPS array

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -29,9 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'rest_framework',
     'corsheaders',
+    'django_extensions',
     
     'attempts',
     'audit_logs',
