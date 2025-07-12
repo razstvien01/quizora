59e4bb7def105f8e39efc604d6d27e235f68793d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-13 00:32:46 +0800

    chore: fix tab spacing by 4

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -5,20 +5,20 @@ from backend.mock_quizzes.models import MockQuiz
 
 
 class Tag(models.Model):
-  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-  name = models.CharField(max_length=50, unique=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  
-  def __str__(self):
-    return f"Tag {self.id} - {self.name}"
-  
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Tag {self.id} - {self.name}"
+    
 class MockQuizTag(models.Model):
-  mock_quiz = models.ForeignKey(MockQuiz, on_delete=models.CASCADE)
-  tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-  
-  def __str__(self):
-    return f"MockQuizTag {self.mock_quiz} - {self.tag}"
-  
-  class Meta:
-    unique_together = ('mock_quiz', 'tag')
+    mock_quiz = models.ForeignKey(MockQuiz, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"MockQuizTag {self.mock_quiz} - {self.tag}"
+    
+    class Meta:
+        unique_together = ('mock_quiz', 'tag')
