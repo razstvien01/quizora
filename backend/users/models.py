7516a75f89632e429dbfe57351af5a8ba886cfc8
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-16 03:30:39 +0800

    chore: modified emaill, and username

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,11 +5,14 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     auth_id = models.CharField(max_length=128, unique=True)
-    email = models.EmailField()
-    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True, blank=True, null=True)
     role = models.CharField(max_length=64, default='student')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] 
     
     def __str__(self):
         return f"User {self.email} ({self.role})"
