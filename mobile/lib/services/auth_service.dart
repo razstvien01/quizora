9f37fa3dfac9b9d0bc1ba6e752fb78decc793e3d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-17 14:00:28 +0800

    mend
    chore: fix typo

--- mobile/lib/services/auth_service.dart
+++ mobile/lib/services/auth_service.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:quizora/api/auth_api.dart';
 import 'package:quizora/constants/env.dart';
 
 class AuthService {
@@ -13,6 +16,26 @@ class AuthService {
     );
     await storage.write(key: 'access_token', value: credentials.accessToken);
     await storage.write(key: 'id_token', value: credentials.idToken);
+
+    try {
+      final idTokenParts = credentials.idToken.split('.');
+
+      if (idTokenParts.length == 3) {
+        final decoded = utf8.decode(
+          base64Url.decode(base64Url.normalize(idTokenParts[1])),
+        );
+        final payload = json.decode(decoded);
+
+        final email = payload['email'];
+        final username = payload['name'];
+        final authId = payload['sub'];
+
+        await registeringUserToBackend(email, username, authId);
+      }
+    } catch (error) {
+      print(error);
+    }
+
     return credentials;
   }
 
@@ -29,5 +52,24 @@ class AuthService {
 
   Future<String?> getAccessToken() async {
     return await storage.read(key: 'access_token');
+  }
+
+  Future<void> registeringUserToBackend(
+    String email,
+    String username,
+    String authId,
+  ) async {
+    final response = await AuthApi.registerUser({
+      'email': email,
+      'username': username,
+      'role': 'student',
+      'auth_id': authId,
+    });
+
+    if (response.statusCode != 200) {
+      print("Registration failed: ${response.statusMessage}");
+    } else {
+      print("User registered: ${response.data}");
+    }
   }
 }
