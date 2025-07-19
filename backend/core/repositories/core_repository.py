6beca1fdc90a985121d37eed5829126add632e10
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-19 23:49:04 +0800

    chore: refact3ored create_user, by using the user dto parameter

--- backend/core/repositories/core_repository.py
+++ backend/core/repositories/core_repository.py
@@ -1,15 +1,10 @@
-from users.models import User
+from users.models.user import User
 from django.db.models import Q
 
 class CoreRepository:
     @staticmethod
-    def create_user(email, username, role, auth_id):
-        return User.objects.create(
-            email=email,
-            username=username,
-            role=role,
-            auth_id=auth_id
-        )
+    def create_user(user: User):
+        return user.save() or user
         
     @staticmethod
     def update_auth_id(user: User, auth_id: str):
