6caffe1e77b367b0e5846ec1d8dc9aadd28973c7
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-19 23:47:52 +0800

    chore: implemented user mapper

---
+++ backend/users/mappers/user_mapper.py
@@ -0,0 +1,42 @@
+from users.dto.user_dto import UserDto
+from users.models.user import User
+from core.repositories.core_repository import CoreRepository
+    
+class UserMapper:
+    @staticmethod
+    def create_user_from_dto(dto: UserDto) -> User:
+        user = User(
+            email=dto.email,
+            first_name=dto.first_name,
+            last_name=dto.last_name,
+            picture=dto.picture,
+            is_active=dto.is_active,
+            is_super_user=dto.is_super_user,
+            role=dto.role,
+            auth_id=dto.auth_id,
+        )
+        
+        return CoreRepository.create_user(user)
+        
+    @staticmethod
+    def user_model_to_dto(user: User) -> UserDto:
+        return UserDto(
+            email=user.email,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            picture=user.picture,
+            is_active=user.is_active,
+            is_super_user=user.is_super_user,
+            role=user.role,
+            auth_id=user.auth_id
+        )
+        
+    @staticmethod
+    def update_model_from_dto(user: User, dto: UserDto) -> User:
+        user.first_name = dto.first_name
+        user.last_name = dto.last_name
+        user.picture = dto.picture
+        user.is_active = dto.is_active
+        user.is_superuser = dto.is_superuser
+        user.role = dto.role
+        return user
