0902556aedb91a7f92b25ee207eb53444febe031
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-19 23:50:10 +0800

    chore: applied the UserDto in the user view when accepting the user parameters

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,26 +2,32 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from core.services.core_service import CoreService
+from users.dto.user_dto import UserDto
 class AuthUser(APIView):
     permission_classes = [AllowAny]
     
     def post(self, request):
         data = request.data
-        email = data.get("email")
-        username = data.get("username")
-        role = data.get("role", "student")
-        auth_id = data.get("auth_id")
+        user_dto = UserDto(
+            email=data['email'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            picture=data.get('picture'),
+            is_active=True,
+            is_super_user=False,
+            role=data.get('role', 'user'),
+            auth_id=data['auth_id'],
+        )
         
-        if not email or not auth_id:
+        if not user_dto.email or not user_dto.auth_id:
             return Response({
                 "error": "Missing required fields"},
                 status=400
             )
-        
-        user, created = CoreService.get_or_create_user(email, username, role, auth_id)
+            
+        created_user = CoreService.update_or_create_user(user_dto)
         
         return Response({
-            "id": user.id,
-            "email": user.email,
-            "new": created
+            "message": "User created",
+            "user_id": created_user.id 
         })
