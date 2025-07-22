8ec39729489326e492b237b3f81876cd4de796bb
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-22 10:57:59 +0800

    chore: integrate debugpy for remote debugging support in backend Docker setup

--- backend/manage.py
+++ backend/manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import debugpy
+
+print("Waiting for debugger to attach...")
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
