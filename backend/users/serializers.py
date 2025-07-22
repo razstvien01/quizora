e18408bc3c18efe4982ae2d83946df225633907d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-23 03:35:01 +0800

    chore: implemented user serializers

---
+++ backend/users/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from users.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'is_active',
+            'is_superuser',
+            'role'
+        ]
