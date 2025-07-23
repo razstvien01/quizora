519d94d10011b155e8a45dbbfcc22323bf96caa2
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-23 12:16:03 +0800

    chore: added api user path url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls'))
+    path('api/', include('core.urls')),
+    path('api/users/', include('users.urls'))
 ]
 
 if settings.DEBUG:
