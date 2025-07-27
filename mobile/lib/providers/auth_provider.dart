47df3b38f1fb3b03a0c40ead54d2416712814de7
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-28 00:56:17 +0800

    chore: changed data type of class to User

--- mobile/lib/providers/auth_provider.dart
+++ mobile/lib/providers/auth_provider.dart
@@ -1,34 +1,40 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quizora/mappers/user_mapper.dart';
+import 'package:quizora/models/user.dart';
 import 'package:quizora/services/auth_service.dart';
 
 final authServiceProvider = Provider((ref) => AuthService());
 
-final authStateProvider = StateNotifierProvider<AuthController, bool>((ref) {
+final authStateProvider = StateNotifierProvider<AuthController, User?>((ref) {
   final authService = ref.read(authServiceProvider);
   return AuthController(authService);
 });
 
-class AuthController extends StateNotifier<bool> {
+class AuthController extends StateNotifier<User?> {
   final AuthService _authService;
 
-  AuthController(this._authService) : super(false) {
+  AuthController(this._authService) : super(null) {
     _checkLogin();
   }
 
   void _checkLogin() async {
-    final loggedIn = await _authService.isLoggedIn();
-    state = loggedIn;
+    final userDto = await _authService.getCurrentUser();
+    if (userDto != null) {
+      state = UserMapper.fromDto(userDto);
+    }
   }
 
   Future<bool> login() async {
-    final creds = await _authService.login();
-    final isSuccess = creds != null;
-    state = isSuccess;
-    return isSuccess;
+    final userDto = await _authService.login();
+    if (userDto != null) {
+      state = UserMapper.fromDto(userDto);
+      return true;
+    }
+    return false;
   }
 
   Future<void> logout() async {
     await _authService.logout();
-    state = false;
+    state = null;
   }
 }
