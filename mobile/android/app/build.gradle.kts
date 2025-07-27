e92b1645e727f7f39f89035b33af123c56fca6ec
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-28 00:54:11 +0800

    chore: modified package name to com.mobile.quizora

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 
 android {
-    namespace = "com.quizora.mobile"
+    namespace = "com.mobile.quizora"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.quizora.mobile"
+        applicationId = "com.mobile.quizora"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- mobile/android/app/src/main/kotlin/com/mobile/quizora/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/mobile/quizora/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.quizora.mobile
+package com.mobile.quizora
 
 import io.flutter.embedding.android.FlutterActivity
 
