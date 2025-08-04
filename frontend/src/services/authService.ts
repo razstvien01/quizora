b419de2d3387da251d8ea37b0ea5ff5851b841cc
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-08-04 14:32:27 +0800

    feat: upgraded registerUser, added userDto when communicating with the backend

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,14 +1,11 @@
 import { API_ENDPOINTS } from "@/constants/api";
+import type { UserDto } from "@/types/user.type";
 import axios from "axios";
 
-export const registerUser = async (token: string) => {
-  return axios.post(
-    API_ENDPOINTS.AUTH,
-    {},
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+export const registerUser = async (token: string, userDto: UserDto) => {
+  return axios.post(API_ENDPOINTS.AUTH, userDto, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
